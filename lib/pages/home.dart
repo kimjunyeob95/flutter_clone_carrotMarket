174678bfbb36f17c8162c32f877e953dc2759f19
kimjun <kimjun@hanbom.com>
2022-02-17 23:08:27 +0900

    selectbox event 작업

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:carrot_market_sample/pages/repository/contents_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
@@ -10,8 +11,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<Map<String, String>> datas = [];
   late String _currentLocation;
+  final ContentsRepository contentsRepository = ContentsRepository();
   final Map<String, String> locationTypeToString = {
     "성내동": "성내동",
     "천호동": "천호동",
@@ -22,86 +23,19 @@ class _HomeState extends State<Home> {
   void initState() {
     super.initState();
     _currentLocation = "성내동";
-    datas = [
-      {
-        "image": "assets/images/ara-1.jpg",
-        "title": "네메시스 축구화275",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-2.jpg",
-        "title": "LA갈비 5kg팔아요~",
-        "location": "제주 제주시 아라동",
-        "price": "100000",
-        "likes": "5"
-      },
-      {
-        "image": "assets/images/ara-3.jpg",
-        "title": "치약팝니다",
-        "location": "제주 제주시 아라동",
-        "price": "5000",
-        "likes": "0"
-      },
-      {
-        "image": "assets/images/ara-4.jpg",
-        "title": "[풀박스]맥북프로16인치 터치바 스페이스그레이",
-        "location": "제주 제주시 아라동",
-        "price": "2500000",
-        "likes": "6"
-      },
-      {
-        "image": "assets/images/ara-5.jpg",
-        "title": "디월트존기임팩",
-        "location": "제주 제주시 아라동",
-        "price": "150000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-6.jpg",
-        "title": "갤럭시s10",
-        "location": "제주 제주시 아라동",
-        "price": "180000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-7.jpg",
-        "title": "선반",
-        "location": "제주 제주시 아라동",
-        "price": "15000",
-        "likes": "2"
-      },
-      {
-        "image": "assets/images/ara-8.jpg",
-        "title": "냉장 쇼케이스",
-        "location": "제주 제주시 아라동",
-        "price": "80000",
-        "likes": "3"
-      },
-      {
-        "image": "assets/images/ara-9.jpg",
-        "title": "대우 미니냉장고",
-        "location": "제주 제주시 아라동",
-        "price": "30000",
-        "likes": "3"
-      },
-      {
-        "image": "assets/images/ara-10.jpg",
-        "title": "멜킨스 풀업 턱걸이 판매합니다.",
-        "location": "제주 제주시 아라동",
-        "price": "50000",
-        "likes": "7"
-      }
-    ];
   }
 
   final oCcy = NumberFormat("#,###", "ko_KR");
   String calcStringToWon(String priceString) {
+    if (priceString == "무료나눔") return priceString;
     return "${oCcy.format(int.parse(priceString))}원";
   }
 
-  Widget _bodyWidget() {
+  _loadContents() {
+    return contentsRepository.loadContentsFromLocation(_currentLocation);
+  }
+
+  _makeDataList(List<Map<String, String>> datas) {
     return ListView.separated(
         padding: const EdgeInsets.symmetric(horizontal: 10),
         itemBuilder: (BuildContext _context, int index) {
@@ -177,6 +111,30 @@ class _HomeState extends State<Home> {
           );
         },
         itemCount: datas.length);
+  }
+
+  Widget _bodyWidget() {
+    return FutureBuilder<List<Map<String, String>>>(
+        future: _loadContents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('오류'),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return _makeDataList(snapshot.data ?? []);
+          }
+
+          return const Center(
+            child: Text('해당 지역에 데이터가 없습니다.'),
+          );
+        });
   }
 
   PreferredSizeWidget _appbarWidget() {
